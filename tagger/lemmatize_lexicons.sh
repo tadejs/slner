ae91cc9bf3b8c74afa4533002b87d138c4bb405b
Tadej Štajner <tadej.stajner@ijs.si>
2012-01-13 22:06:37 +0100

    lemmatized lexicons

--- tagger/lemmatize_lexicons.sh
+++ tagger/lemmatize_lexicons.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find ../lexicons -name "*sl.txt" | xargs -P 4 -I {} sh tag.sh {} {}.tei.xml
-find ../lexicons -name "*.tei.xml" | xargs -P 4 -I {} python lemma.py {} {}.lemmas.txt
+find ../lexicons -name "*sl.txt" | xargs -P 2 -I {} sh tag.sh {} {}.tei.xml
+find ../lexicons -name "*.tei.xml" | xargs -P 2 -I {} python lemmas.py {} {}.lemmas.txt
 
--- tagger/tag.sh
+++ tagger/tag.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mono PosTaggerTag.exe -v -lem:lemmatizer-model.bin $1 tagger-model.bin $2
+mono -O=all PosTaggerTag.exe -v -lem:lemmatizer-model.bin $1 tagger-model.bin $2
 
