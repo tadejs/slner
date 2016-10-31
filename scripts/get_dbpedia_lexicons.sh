9b3d473bb3a1380c958cb95a5cd43e20a011be02
Oto Brglez <otobrglez@gmail.com>
2016-10-31 18:59:44 +0100

    Moving scripts around.

--- scripts/get_dbpedia_lexicons.sh
+++
@@ -1,2 +0,0 @@
-4s-query --soft-limit 5000 dbpsl 'SELECT ?s ?nm WHERE { ?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/Organisation>. ?s <http://www.w3.org/2000/01/rdf-schema#label> ?nm . } LIMIT 500 #EOQ'
-
